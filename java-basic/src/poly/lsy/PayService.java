8530550298578c78a5881bdd80cb1a596b561338
이상엽 <infightersy@naver.com>
2024-10-28 11:29:18 +0900

    추가 중

---
+++ java-basic/src/poly/lsy/PayService.java
@@ -0,0 +1,16 @@
+package poly.lsy;
+
+
+public class PayService {
+
+    private Pay pay;
+
+    public void processPay(String option, int amount) {
+
+        boolean result;
+        System.out.println("결제를 시작합니다: option=" + option + ", amount=" + amount);
+
+
+
+    }
+}
